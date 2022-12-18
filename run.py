2ad7e07ed5a0e01aae3dc38b1aab5711a79c3d52
Kieran Collins <kiecol@protonmail.com>
2022-12-18 11:21:48 +0000

    Set up APIs and access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials 
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('python_quiz')
+
+score_tracker = SHEET.worksheet("ScoreTracker")
+leader_board = SHEET.worksheet("LeaderBoard")
+
+scores = score_tracker.get_all_values()
+leaders = leader_board.get_all_values()
+
+print(f"Score Tracker:\n{scores}")
+print(f"Leader Board:\n{leaders}")
