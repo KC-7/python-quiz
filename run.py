bb14944926cb240ef43f0a93c50cea30ff10ff59
Kieran Collins <kiecol@protonmail.com>
2022-12-19 19:50:52 +0000

    Replace placeholder text with actual questions and options

--- run.py
+++ run.py
@@ -15,28 +15,28 @@ SHEET = GSPREAD_CLIENT.open('python_quiz')
 
 
 QUESTIONS = [{
-    "question": "Sample Question Text 1",
+    "question": "What is largest of the below file sizes?",
     "options": [
-        "Option 1: QWERTY", "Option 2: QWERTY",
-        "Option 3: QWERTY", "Option 4: QWERTY"],
+        "Option 1: 1TB", "Option 2: 1MB",
+        "Option 3: 1GB", "Option 4: 1KB"],
     "answer": 1
 }, {
-    "question": "Sample Question Text 2",
+    "question": "How much did the first computer weigh?",
     "options": [
-        "Option 1: QWERTY", "Option 2: QWERTY",
-        "Option 3: QWERTY", "Option 4: QWERTY"],
+        "Option 1: 27 grams", "Option 2: 27 ton",
+        "Option 3: 27 pounds", "Option 4: 27 stone"],
     "answer": 2
 }, {
-    "question": "Sample Question Text 3",
+    "question": "The first known computer programmer was a:",
     "options": [
-        "Option 1: QWERTY", "Option 2: QWERTY",
-        "Option 3: QWERTY", "Option 4: QWERTY"],
+        "Option 1: dog", "Option 2: man",
+        "Option 3: woman", "Option 4: teenager"],
     "answer": 3
 }, {
-    "question": "Sample Question Text 4",
+    "question": "How much did the first 1GB hard drive cost?",
     "options": [
-        "Option 1: QWERTY", "Option 2: QWERTY",
-        "Option 3: QWERTY", "Option 4: QWERTY"],
+        "Option 1: $400", "Option 2: $4,000,000",
+        "Option 3: $4,000", "Option 4: $40,000"],
     "answer": 4
 }]
 
